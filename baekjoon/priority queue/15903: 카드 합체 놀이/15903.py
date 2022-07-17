6564243833bb318228b8bae8395ffb734588463a
KMUsungwon <sungwontoto@kookmin.ac.kr>
2022-07-17 19:08:37 +0900

    solving algorithm

---
+++ baekjoon/priority queue/15903: 카드 합체 놀이/15903.py
@@ -0,0 +1,14 @@
+import heapq
+n, m = map(int, input().split())
+cards = list(map(int, input().split()))
+
+heapq.heapify(cards)
+
+for _ in range(m):
+    a = heapq.heappop(cards)
+    b = heapq.heappop(cards)
+
+    heapq.heappush(cards, a+b)
+    heapq.heappush(cards, a+b)
+
+print(sum(cards))
